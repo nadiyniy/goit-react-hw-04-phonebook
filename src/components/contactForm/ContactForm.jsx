a08359db99e6a8a27b10072936a251c4d4698ea5
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-10-27 09:48:30 +0300

    upadte PB on hooks

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import { StyledContactForm } from './ContactForm.styled';
 import propTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
-class ContactForm extends React.Component {
-  static propTypes = {
-    onAddContact: propTypes.func.isRequired,
-    contacts: propTypes.array.isRequired,
+const ContactForm = ({ onAddContact, contacts }) => {
+  // state = {
+  //   name: '',
+  //   number: '',
+  // };
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handelOnChange = e => {
+    const { name, value } = e.target;
+
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  state = {
-    name: '',
-    number: '',
-  };
-
-  handelOnChange = e => {
-    const { target } = e;
-    const { name, value } = target;
-    this.setState({ [name]: value });
-  };
-
-  handelOnSubmit = e => {
+  const handelOnSubmit = e => {
     e.preventDefault();
 
-    const { name, number } = this.state;
-    const { contacts, onAddContact } = this.props;
+    // const { name, number } = this.state;
+    // const { contacts, onAddContact } = this.props;
 
     const newContact = {
       number: number.trim(),
@@ -37,41 +39,42 @@ class ContactForm extends React.Component {
 
     onAddContact(newContact);
 
-    this.setState({
-      contacts: [newContact, ...contacts],
-      name: '',
-      number: '',
-    });
+    // contacts = [newContact, ...contacts];
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <StyledContactForm onSubmit={this.handelOnSubmit}>
-        <label>
-          Name:
-          <input
-            onChange={this.handelOnChange}
-            value={this.state.name}
-            placeholder="Enter name"
-            name="name"
-            required
-          />
-        </label>
-        <label>
-          Number:
-          <input
-            onChange={this.handelOnChange}
-            value={this.state.number}
-            placeholder="number"
-            type="tel"
-            name="number"
-            required
-          />
-        </label>
-        <button>Add contact</button>
-      </StyledContactForm>
-    );
-  }
-}
+  return (
+    <StyledContactForm onSubmit={handelOnSubmit}>
+      <label>
+        Name:
+        <input
+          onChange={handelOnChange}
+          value={name}
+          placeholder="Enter name"
+          name="name"
+          required
+        />
+      </label>
+      <label>
+        Number:
+        <input
+          onChange={handelOnChange}
+          value={number}
+          placeholder="number"
+          type="tel"
+          name="number"
+          required
+        />
+      </label>
+      <button>Add contact</button>
+    </StyledContactForm>
+  );
+};
+
+ContactForm.propTypes = {
+  onAddContact: propTypes.func.isRequired,
+  contacts: propTypes.array.isRequired,
+};
 
 export default ContactForm;
